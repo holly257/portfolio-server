37c9863ab15b0b5abd3f500fde09da84232476e1
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-08-24 16:54:28 -0400

    moved cors

--- src/app.js
+++ src/app.js
@@ -14,9 +14,9 @@ const morganOption = (NODE_ENV === 'production')
     ? 'tiny'
     : 'common';
 
+app.use(cors())
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello world')
